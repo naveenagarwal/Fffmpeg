9a3076768e50b165582cf8af608cae932eda4015
Naveen <naveen@naveen-HP-Pavilion-g6-Notebook-PC.(none)>
2012-08-13 13:57:38 +0530

    blank structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ffmpeg::Application.config.secret_token = '59729bf97f84eb260cca3e002781bf32e6d9f81009b899893d68a302d4b8ac2e8d210db271db8cc76fde6adc9a5f4647bd0104de7658a72ebf4e8b500796a1d5'
